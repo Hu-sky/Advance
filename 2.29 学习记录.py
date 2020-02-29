0fc63f6b3585c0b4972c441153080ab9b71db494
Hu-sky <Mr.Husky@foxmail.com>
2020-02-29 21:37:01 +0800

    Create 2.29 学习记录.py

---
+++ 2.29 学习记录.py
@@ -0,0 +1,8 @@
+# 1.进程间的通信用Queue、Pips等实现
+# 2.除法的应用：调换整数位置
+# num = int(input('Please enter a number:'))
+# re_num = 0
+# while num > 0:
+#     re_num = re_num * 10 + num % 10
+#     num //= 10
+# print(re_num)
